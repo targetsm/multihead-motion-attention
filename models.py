9d392b0e9117ee2083987f5c81685923157399dd
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2021-06-04 17:26:19 +0200

    working transformer model

--- models.py
+++ models.py
@@ -313,36 +313,28 @@ class TransformerModel(BaseModel):
 class CustomTransformer(BaseModel):
 
     def __init__(self, config):
-        super(AttModel, self).__init__(config)
+        super(CustomTransformer, self).__init__(config)
 
         print(config)
         in_features = 135
         kernel_size = 10  # M
-        d_model = 4590
         num_stage = 2
         dct_n = 34
+        d_model = dct_n * in_features
 
         self.kernel_size = kernel_size
         self.d_model = d_model
         self.dct_n = dct_n
 
-        self.convQ = nn.Sequential(nn.Conv1d(in_channels=in_features, out_channels=d_model, kernel_size=6,
-                                             bias=False),
-                                   nn.ReLU(),
-                                   nn.Conv1d(in_channels=d_model, out_channels=d_model, kernel_size=5,
-                                             bias=False),
-                                   nn.ReLU())
-
-        self.convK = nn.Sequential(nn.Conv1d(in_channels=in_features, out_channels=d_model, kernel_size=6,
-                                             bias=False),
-                                   nn.ReLU(),
-                                   nn.Conv1d(in_channels=d_model, out_channels=d_model, kernel_size=5,
-                                             bias=False),
-                                   nn.ReLU())
-
-        self.gcn = GCN.GCN(input_feature=(dct_n) * 2, hidden_feature=d_model, p_dropout=0.3,
-                           num_stage=num_stage,
-                           node_n=in_features)
+        self.transformer = torch.nn.Transformer(d_model=dct_n*in_features,
+                                                nhead=1,
+                                                num_encoder_layers=1,
+                                                num_decoder_layers=1,
+                                                dim_feedforward=10,
+                                                dropout=0.1,
+                                                activation='relu',
+                                                custom_encoder=None,
+                                                custom_decoder=None)
 
     def create_model(self):
         pass
@@ -356,73 +348,37 @@ class CustomTransformer(BaseModel):
 
         model_out = {'seed': batch.poses[:, :self.config.seed_seq_len],
                      'predictions': None}
-        batch_size = batch.batch_size
+        bs = batch.batch_size
         src = batch.poses
         output_n = 24  # number of output frames T
         input_n = 120  # number of input frames N
         itera = 1
 
-        # construct keys and queries
-        dct_n = self.dct_n
-        src = src[:, :input_n]  # remove given output frames
-        src_tmp = src.clone()  # shape:[16, 120, 135]=[batch_size, input_n, in_features]
-        bs = src.shape[0]  # batch_size
-        src_key_tmp = src_tmp.transpose(1, 2)[:, :, :(input_n - output_n)].clone()  # construct keys, shape:[16, 135, 96]
-        src_query_tmp = src_tmp.transpose(1, 2)[:, :, -self.kernel_size:].clone()  # construct queries, shape:[16, 135, 10]
+        src_tmp = src.clone()  # torch.Size([16, 144, 135])
+        src_transformer = src_tmp[:,:input_n,:].clone()  # torch.Size([16, 120, 135])
+        tgt_transformer = src_tmp[:,-output_n-self.kernel_size:,:].clone()  # torch.Size([16, 34, 135])
+        #print(src_transformer.shape, tgt_transformer.shape)
 
-        # get dct matrices
-        dct_m, idct_m = util.get_dct_matrix(self.kernel_size + output_n)
-        dct_m = torch.from_numpy(dct_m).float().cuda()
-        idct_m = torch.from_numpy(idct_m).float().cuda()
-
-        # construct values and apply cosine transform
         vn = input_n - self.kernel_size - output_n + 1  # N - M - T + 1 = 87
         vl = self.kernel_size + output_n  # M + T = 34
         idx = np.expand_dims(np.arange(vl), axis=0) + \
               np.expand_dims(np.arange(vn), axis=1)
-        src_value_tmp = src_tmp[:, idx].clone().reshape(
-            [bs * vn, vl, -1])  # shape:[1392, 34, 135]
-        src_value_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), src_value_tmp).reshape(
-            [bs, vn, dct_n, -1]).transpose(2, 3).reshape(
-            [bs, vn, -1])  # shape:[16, 87, 34*135]
+        #print(idx)
+        #print(src_transformer.shape)
+        src_transformer = src_transformer[:, idx].reshape(
+            [bs * vn, vl, -1])  # torch.Size([1552, 34, 135])
+        #print(src_transformer.shape)
+        src_transformer = src_transformer.reshape(
+            [bs, vn, -1]).transpose(0,1)  # shape:[16, 97, 34*135]
 
-        idx = list(range(-self.kernel_size, 0, 1)) + [-1] * output_n
-        outputs = []
 
-        key_tmp = self.convK(src_key_tmp / 1000.0)  # shape:[16, 512, 87] input embedding
-        for i in range(itera):
-            # Motion Attention
-            query_tmp = self.convQ(src_query_tmp / 1000.0)  # shape:[16, 512, 1] input embedding
-            #score_tmp = torch.matmul(query_tmp.transpose(1, 2), key_tmp) + 1e-15
-            #att_tmp = score_tmp / (torch.sum(score_tmp, dim=2, keepdim=True))
-            #dct_att_tmp = torch.matmul(att_tmp, src_value_tmp)[:, 0].reshape(
-            #    [bs, -1, dct_n])
-            #print(dct_att_tmp.shape, torch.matmul(att_tmp, src_value_tmp).shape)
+        tgt_transformer = tgt_transformer.reshape([bs, 1, -1]).transpose(0,1)
+        #print(src_transformer.shape, tgt_transformer.shape)
 
-            device = torch.device("cuda:0")
-            query_tmp = query_tmp.reshape([1, 16, 4590])
-            key_tmp = key_tmp.reshape([87, 16, 4590])
-            src_value_tmp = src_value_tmp.reshape([87, 16, 4590])
-            multihead_attn = nn.MultiheadAttention(4590, 1).to(device)
-            #print(query_tmp.shape, key_tmp.shape, src_value_tmp.shape)
-            dct_att_tmp, attn_output_weights = multihead_attn(query_tmp, key_tmp, src_value_tmp)
-            dct_att_tmp = (dct_att_tmp.transpose(0,1))[:, 0].reshape(
-                [bs, -1, dct_n])
-            #print(dct_att_tmp.shape)
-            input_gcn = src_tmp[:, idx]  # shape:[16, 34, 135]
-            dct_in_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), input_gcn).transpose(1, 2)
-            dct_in_tmp = torch.cat([dct_in_tmp, dct_att_tmp], dim=-1)
-            dct_out_tmp = self.gcn(dct_in_tmp)
-            out_gcn = torch.matmul(idct_m[:, :dct_n].unsqueeze(dim=0),
-                                   dct_out_tmp[:, :, :dct_n].transpose(1, 2))
-            print(out_gcn.shape)
-            outputs.append(out_gcn.unsqueeze(2))
-
-        outputs = torch.cat(outputs, dim=2)
-
-        out_sq = outputs.squeeze()
-        out_sq = out_sq[:, -24:, :]
-        model_out['predictions'] = out_sq.reshape(batch_size, self.config.target_seq_len, -1)
+        output = self.transformer(src_transformer, tgt_transformer).cuda()
+        #print(output.shape)
+        output = output.transpose(0,1).reshape([bs, 34, 135])
+        model_out['predictions'] = output[:, -24:, :]
         return model_out
 
     def backward(self, batch: AMASSBatch, model_out):
