09f955c436e3f577131d9a29b2794f95d641c72b
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2021-06-04 09:09:26 +0200

    added multihead attention, not working

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ from model import GCN, util
 def create_model(config):
     # This is a helper function that can be useful if you have several model definitions that you want to
     # choose from via the command line. For now, we just return the Dummy model.
-    return TransformerModel(config)
+    return CustomTransformer(config)
 
 
 class BaseModel(nn.Module):
@@ -106,7 +106,7 @@ class AttModel(BaseModel):
         print(config)
         in_features = 135
         kernel_size = 10  # M
-        d_model = 512
+        d_model = 4590
         num_stage = 2
         dct_n = 34
 
@@ -177,44 +177,34 @@ class AttModel(BaseModel):
         idx = list(range(-self.kernel_size, 0, 1)) + [-1] * output_n
         outputs = []
 
-        key_tmp = self.convK(src_key_tmp / 1000.0)  # shape:[16, 512, 87]
+        key_tmp = self.convK(src_key_tmp / 1000.0)  # shape:[16, 512, 87] input embedding
         for i in range(itera):
             # Motion Attention
-            query_tmp = self.convQ(src_query_tmp / 1000.0)  # shape:[16, 512, 1]
-            score_tmp = torch.matmul(query_tmp.transpose(1, 2), key_tmp) + 1e-15
-            att_tmp = score_tmp / (torch.sum(score_tmp, dim=2, keepdim=True))
-            dct_att_tmp = torch.matmul(att_tmp, src_value_tmp)[:, 0].reshape(
-                [bs, -1, dct_n])
+            query_tmp = self.convQ(src_query_tmp / 1000.0)  # shape:[16, 512, 1] input embedding
+            #score_tmp = torch.matmul(query_tmp.transpose(1, 2), key_tmp) + 1e-15
+            #att_tmp = score_tmp / (torch.sum(score_tmp, dim=2, keepdim=True))
+            #dct_att_tmp = torch.matmul(att_tmp, src_value_tmp)[:, 0].reshape(
+            #    [bs, -1, dct_n])
+            #print(dct_att_tmp.shape, torch.matmul(att_tmp, src_value_tmp).shape)
 
+            device = torch.device("cuda:0")
+            query_tmp = query_tmp.reshape([1, 16, 4590])
+            key_tmp = key_tmp.reshape([87, 16, 4590])
+            src_value_tmp = src_value_tmp.reshape([87, 16, 4590])
+            multihead_attn = nn.MultiheadAttention(4590, 1).to(device)
+            #print(query_tmp.shape, key_tmp.shape, src_value_tmp.shape)
+            dct_att_tmp, attn_output_weights = multihead_attn(query_tmp, key_tmp, src_value_tmp)
+            dct_att_tmp = (dct_att_tmp.transpose(0,1))[:, 0].reshape(
+                [bs, -1, dct_n])
+            #print(dct_att_tmp.shape)
             input_gcn = src_tmp[:, idx]  # shape:[16, 34, 135]
             dct_in_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), input_gcn).transpose(1, 2)
             dct_in_tmp = torch.cat([dct_in_tmp, dct_att_tmp], dim=-1)
             dct_out_tmp = self.gcn(dct_in_tmp)
             out_gcn = torch.matmul(idct_m[:, :dct_n].unsqueeze(dim=0),
                                    dct_out_tmp[:, :, :dct_n].transpose(1, 2))
+            print(out_gcn.shape)
             outputs.append(out_gcn.unsqueeze(2))
-            if itera > 1:
-                # update key-value query
-                out_tmp = out_gcn.clone()[:, 0 - output_n:]
-                src_tmp = torch.cat([src_tmp, out_tmp], dim=1)
-
-                vn = 1 - 2 * self.kernel_size - output_n
-                vl = self.kernel_size + output_n
-                idx_dct = np.expand_dims(np.arange(vl), axis=0) + \
-                          np.expand_dims(np.arange(vn, -self.kernel_size - output_n + 1), axis=1)
-
-                src_key_tmp = src_tmp[:, idx_dct[0, :-1]].transpose(1, 2)
-                key_new = self.convK(src_key_tmp / 1000.0)
-                key_tmp = torch.cat([key_tmp, key_new], dim=2)
-
-                src_dct_tmp = src_tmp[:, idx_dct].clone().reshape(
-                    [bs * self.kernel_size, vl, -1])
-                src_dct_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), src_dct_tmp).reshape(
-                    [bs, self.kernel_size, dct_n, -1]).transpose(2, 3).reshape(
-                    [bs, self.kernel_size, -1])
-                src_value_tmp = torch.cat([src_value_tmp, src_dct_tmp], dim=1)
-
-                src_query_tmp = src_tmp[:, -self.kernel_size:].transpose(1, 2)
 
         outputs = torch.cat(outputs, dim=2)
 
@@ -304,6 +294,144 @@ class TransformerModel(BaseModel):
         pass
 
     def backward(self, batch: AMASSBatch, model_out):
+        predictions = model_out['predictions']
+        targets = batch.poses[:, -24:]
+
+        total_loss = mse(predictions, targets)
+
+        # If you have more than just one loss, just add them to this dict and they will automatically be logged.
+        loss_vals = {'total_loss': total_loss.cpu().item()}
+
+        if self.training:
+            # We only want to do backpropagation in training mode, as this function might also be called when evaluating
+            # the model on the validation set.
+            total_loss.backward()
+
+        return loss_vals, targets
+
+
+class CustomTransformer(BaseModel):
+
+    def __init__(self, config):
+        super(AttModel, self).__init__(config)
+
+        print(config)
+        in_features = 135
+        kernel_size = 10  # M
+        d_model = 4590
+        num_stage = 2
+        dct_n = 34
+
+        self.kernel_size = kernel_size
+        self.d_model = d_model
+        self.dct_n = dct_n
+
+        self.convQ = nn.Sequential(nn.Conv1d(in_channels=in_features, out_channels=d_model, kernel_size=6,
+                                             bias=False),
+                                   nn.ReLU(),
+                                   nn.Conv1d(in_channels=d_model, out_channels=d_model, kernel_size=5,
+                                             bias=False),
+                                   nn.ReLU())
+
+        self.convK = nn.Sequential(nn.Conv1d(in_channels=in_features, out_channels=d_model, kernel_size=6,
+                                             bias=False),
+                                   nn.ReLU(),
+                                   nn.Conv1d(in_channels=d_model, out_channels=d_model, kernel_size=5,
+                                             bias=False),
+                                   nn.ReLU())
+
+        self.gcn = GCN.GCN(input_feature=(dct_n) * 2, hidden_feature=d_model, p_dropout=0.3,
+                           num_stage=num_stage,
+                           node_n=in_features)
+
+    def create_model(self):
+        pass
+
+    def forward(self, batch: AMASSBatch):
+        """
+        The forward pass.
+        :param batch: Current batch of data.
+        :return: Each forward pass must return a dictionary with keys {'seed', 'predictions'}.
+        """
+
+        model_out = {'seed': batch.poses[:, :self.config.seed_seq_len],
+                     'predictions': None}
+        batch_size = batch.batch_size
+        src = batch.poses
+        output_n = 24  # number of output frames T
+        input_n = 120  # number of input frames N
+        itera = 1
+
+        # construct keys and queries
+        dct_n = self.dct_n
+        src = src[:, :input_n]  # remove given output frames
+        src_tmp = src.clone()  # shape:[16, 120, 135]=[batch_size, input_n, in_features]
+        bs = src.shape[0]  # batch_size
+        src_key_tmp = src_tmp.transpose(1, 2)[:, :, :(input_n - output_n)].clone()  # construct keys, shape:[16, 135, 96]
+        src_query_tmp = src_tmp.transpose(1, 2)[:, :, -self.kernel_size:].clone()  # construct queries, shape:[16, 135, 10]
+
+        # get dct matrices
+        dct_m, idct_m = util.get_dct_matrix(self.kernel_size + output_n)
+        dct_m = torch.from_numpy(dct_m).float().cuda()
+        idct_m = torch.from_numpy(idct_m).float().cuda()
+
+        # construct values and apply cosine transform
+        vn = input_n - self.kernel_size - output_n + 1  # N - M - T + 1 = 87
+        vl = self.kernel_size + output_n  # M + T = 34
+        idx = np.expand_dims(np.arange(vl), axis=0) + \
+              np.expand_dims(np.arange(vn), axis=1)
+        src_value_tmp = src_tmp[:, idx].clone().reshape(
+            [bs * vn, vl, -1])  # shape:[1392, 34, 135]
+        src_value_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), src_value_tmp).reshape(
+            [bs, vn, dct_n, -1]).transpose(2, 3).reshape(
+            [bs, vn, -1])  # shape:[16, 87, 34*135]
+
+        idx = list(range(-self.kernel_size, 0, 1)) + [-1] * output_n
+        outputs = []
+
+        key_tmp = self.convK(src_key_tmp / 1000.0)  # shape:[16, 512, 87] input embedding
+        for i in range(itera):
+            # Motion Attention
+            query_tmp = self.convQ(src_query_tmp / 1000.0)  # shape:[16, 512, 1] input embedding
+            #score_tmp = torch.matmul(query_tmp.transpose(1, 2), key_tmp) + 1e-15
+            #att_tmp = score_tmp / (torch.sum(score_tmp, dim=2, keepdim=True))
+            #dct_att_tmp = torch.matmul(att_tmp, src_value_tmp)[:, 0].reshape(
+            #    [bs, -1, dct_n])
+            #print(dct_att_tmp.shape, torch.matmul(att_tmp, src_value_tmp).shape)
+
+            device = torch.device("cuda:0")
+            query_tmp = query_tmp.reshape([1, 16, 4590])
+            key_tmp = key_tmp.reshape([87, 16, 4590])
+            src_value_tmp = src_value_tmp.reshape([87, 16, 4590])
+            multihead_attn = nn.MultiheadAttention(4590, 1).to(device)
+            #print(query_tmp.shape, key_tmp.shape, src_value_tmp.shape)
+            dct_att_tmp, attn_output_weights = multihead_attn(query_tmp, key_tmp, src_value_tmp)
+            dct_att_tmp = (dct_att_tmp.transpose(0,1))[:, 0].reshape(
+                [bs, -1, dct_n])
+            #print(dct_att_tmp.shape)
+            input_gcn = src_tmp[:, idx]  # shape:[16, 34, 135]
+            dct_in_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), input_gcn).transpose(1, 2)
+            dct_in_tmp = torch.cat([dct_in_tmp, dct_att_tmp], dim=-1)
+            dct_out_tmp = self.gcn(dct_in_tmp)
+            out_gcn = torch.matmul(idct_m[:, :dct_n].unsqueeze(dim=0),
+                                   dct_out_tmp[:, :, :dct_n].transpose(1, 2))
+            print(out_gcn.shape)
+            outputs.append(out_gcn.unsqueeze(2))
+
+        outputs = torch.cat(outputs, dim=2)
+
+        out_sq = outputs.squeeze()
+        out_sq = out_sq[:, -24:, :]
+        model_out['predictions'] = out_sq.reshape(batch_size, self.config.target_seq_len, -1)
+        return model_out
+
+    def backward(self, batch: AMASSBatch, model_out):
+        """
+        The backward pass.
+        :param batch: The same batch of data that was passed into the forward pass.
+        :param model_out: Whatever the forward pass returned.
+        :return: The loss values for book-keeping, as well as the targets for convenience.
+        """
         predictions = model_out['predictions']
         targets = batch.poses[:, -24:]
 
